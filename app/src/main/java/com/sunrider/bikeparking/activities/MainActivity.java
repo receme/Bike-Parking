c5796c91dfaf3598a25635e5267d6157af836f63
tahmid <tahmid1989@gmail.com>
2017-06-12 02:28:39 +0600

    code cleaned

--- app/src/main/java/com/sunrider/bikeparking/activities/MainActivity.java
+++ app/src/main/java/com/sunrider/bikeparking/activities/MainActivity.java
@@ -1,6 +1,5 @@
 package com.sunrider.bikeparking.activities;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
@@ -17,14 +16,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.sunrider.bikeparking.R;
-import com.sunrider.bikeparking.fragments.HomeFragment;
-import com.sunrider.bikeparking.fragments.ContributionFragment;
 import com.sunrider.bikeparking.fragments.AboutFragment;
+import com.sunrider.bikeparking.fragments.ContributionFragment;
+import com.sunrider.bikeparking.fragments.HomeFragment;
 import com.sunrider.bikeparking.fragments.InstructionFragment;
 import com.sunrider.bikeparking.fragments.PrivacyPolicyFragment;
 import com.sunrider.bikeparking.fragments.SettingsFragment;
@@ -34,20 +32,12 @@ public class MainActivity extends AppCompatActivity {
     private NavigationView navigationView;
     private DrawerLayout drawer;
     private View navHeader;
-    private ImageView imgNavHeaderBg, imgProfile;
-    private TextView txtName, txtWebsite;
+    private ImageView imgNavHeaderBg;
     private Toolbar toolbar;
     private FloatingActionButton fab;
 
-    // urls to load navigation header background image
-    // and profile image
-    //private static final String urlNavHeaderBg = "http://api.androidhive.info/images/nav-menu-header-bg.jpg";
-    //private static final String urlProfileImg = "https://lh3.googleusercontent.com/eCtE_G34M9ygdkmOpYvCag1vBARCmZwnVS6rS5t4JLzJ6QgQSBquM0nuTsCpLhYbKljoyS-txg";
-
-    // index to identify current nav menu item
     public static int navItemIndex = 0;
 
-    // tags used to attach the fragments
     private static final String TAG_HOME = "home";
     private static final String TAG_CONTRIBUTIONS = "contributions";
     private static final String TAG_SETTINGS = "settings";
@@ -56,10 +46,8 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG_ABOUT = "about";
     public static String CURRENT_TAG = TAG_HOME;
 
-    // toolbar titles respected to selected nav menu item
     private String[] activityTitles;
 
-    // flag to load home fragment when user presses back key
     private boolean shouldLoadHomeFragOnBackPress = true;
     private Handler mHandler;
 
@@ -79,10 +67,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Navigation view header
         navHeader = navigationView.getHeaderView(0);
-        txtName = (TextView) navHeader.findViewById(R.id.name);
-        txtWebsite = (TextView) navHeader.findViewById(R.id.website);
         imgNavHeaderBg = (ImageView) navHeader.findViewById(R.id.img_header_bg);
-        imgProfile = (ImageView) navHeader.findViewById(R.id.img_profile);
 
         // load toolbar titles from string resources
         activityTitles = getResources().getStringArray(R.array.nav_item_activity_titles);
@@ -114,29 +99,11 @@ public class MainActivity extends AppCompatActivity {
      * name, website, notifications action view (dot)
      */
     private void loadNavHeader() {
-        // name, website
-        //txtName.setText("Ravi Tamada");
-        //txtWebsite.setText("www.androidhive.info");
-
-        // loading header background image
 
         Glide.with(this).asDrawable()
                 .load(R.drawable.nav_menu_header_bg)
                 .into(imgNavHeaderBg);
 
-//        Glide.with(this).load(urlNavHeaderBg)
-//                //.crossFade()
-//                //.diskCacheStrategy(DiskCacheStrategy.ALL)
-//                .into(imgNavHeaderBg);
-
-        // Loading profile image
-//        Glide.with(this).load(urlProfileImg)
-//                .thumbnail(0.5f)
-//                //.bitmapTransform(new CircleTransform(this))
-//                //.diskCacheStrategy(DiskCacheStrategy.ALL)
-//                .into(imgProfile);
-
-        // showing dot next to notifications label
         navigationView.getMenu().getItem(3).setActionView(R.layout.menu_dot);
     }
 
@@ -194,6 +161,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private Fragment getHomeFragment() {
+
         switch (navItemIndex) {
             case 0:
                 // home
@@ -266,16 +234,6 @@ public class MainActivity extends AppCompatActivity {
                         navItemIndex = 5;
                         CURRENT_TAG = TAG_ABOUT;
                         break;
-//                    case R.id.nav_about_us:
-//                        // launch new intent instead of loading fragment
-//                        startActivity(new Intent(MainActivity.this, AboutUsActivity.class));
-//                        drawer.closeDrawers();
-//                        return true;
-//                    case R.id.nav_privacy_policy:
-//                        // launch new intent instead of loading fragment
-//                        startActivity(new Intent(MainActivity.this, PrivacyPolicyActivity.class));
-//                        drawer.closeDrawers();
-//                        return true;
                     default:
                         navItemIndex = 0;
                 }
@@ -342,44 +300,24 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
 
         // show menu only when home fragment is selected
         if (navItemIndex == 0) {
             getMenuInflater().inflate(R.menu.main, menu);
         }
 
-//        // when fragment is notifications, load the menu created for notifications
-//        if (navItemIndex == 3) {
-//            getMenuInflater().inflate(R.menu.notifications, menu);
-//        }
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_logout) {
             Toast.makeText(getApplicationContext(), "Logout user!", Toast.LENGTH_LONG).show();
             return true;
         }
-
-        // user is in notifications fragment
-        // and selected 'Mark all as Read'
-//        if (id == R.id.action_mark_all_read) {
-//            Toast.makeText(getApplicationContext(), "All notifications marked as read!", Toast.LENGTH_LONG).show();
-//        }
-
-        // user is in notifications fragment
-        // and selected 'Clear All'
-//        if (id == R.id.action_clear_notifications) {
-//            Toast.makeText(getApplicationContext(), "Clear all notifications!", Toast.LENGTH_LONG).show();
-//        }
 
         return super.onOptionsItemSelected(item);
     }
---
+++ app/src/main/java/com/sunrider/bikeparking/activities/NavigationDrawerManager.java
@@ -0,0 +1,11 @@
+package com.sunrider.bikeparking.activities;
+
+
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+
+public class NavigationDrawerManager {
+
+    private NavigationView navigationView;
+    private DrawerLayout drawer;
+}
